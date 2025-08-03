59e8245a2736e771d07f3ff79dc3af749e9f3d21
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-08-02 22:34:43 -0300

    finalization

---
+++ notas-estudantis/src/components/StudentTable.tsx
@@ -0,0 +1,47 @@
+import type { Student } from "../types/Student";
+
+
+type Props = {
+    students: Student[];
+
+}
+export const StudentTable = ({ students }: Props) => {
+    return (
+
+        <table className="w-full border border-gray-600  rounded-md  overflow-hidden" >
+            <thead>
+                <tr className="text-left border-b border-gray-600 bg-gray-800">
+                    <th className="p-3">Name</th>
+                    <th className="p-3">Status</th>
+                    <th className="p-3">Grade 1</th>
+                    <th className="p-3">Grade 4</th>
+                    <th className="p-3">Final Grade</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    students.map(item => (
+                        <tr key={item.id} className="text-gray-800 bg-gray-400 border-b border-gray-600">
+                            <td className="p-3 flex items-center">
+                                <img src={item.avatar} alt={item.name} className="w-20 h-20 rounded-full mr-3" />
+                                <div>
+                                    <div className="font-bold">{item.name}</div>
+                                    <div>{item.email}</div>
+                                </div>
+                            </td>
+                            <td className="p-3">
+                                {item.active && <div className="border-green-800 bg-green-600 text-white text-xs rounded-md inline-block px-2 py-1 ">Active</div>}
+                                {!item.active && <div className="border-red-800 bg-red-600 text-white rounded-md text-xs inline-block text-center px-2 py-1">Inactive</div>}
+                            </td>
+                            <td className="p-3">{item.grade1}</td>
+                            <td className="p-3">{item.grade2}</td>
+                            <td className="p-3 font-bold">{item.active ? ((item.grade1 + item.grade2) / 2).toFixed(1) : '--'}</td>
+
+                        </tr>
+                    ))
+                }
+
+            </tbody>
+        </table >
+    );
+}
